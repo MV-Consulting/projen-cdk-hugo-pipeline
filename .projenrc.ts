742bfd0aaed3c12293023d0e147be968a8646981
Manuel Vogel <mavogel@posteo.de>
2023-07-18 07:53:20 +0200

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,16 @@
+import { cdk } from 'projen';
+const project = new cdk.JsiiProject({
+  author: 'Manuel Vogel',
+  authorAddress: 'mavogel@posteo.de',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.0.0',
+  name: 'projen-cdk-hugo-pipeline',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/mavogel/projen-cdk-hugo-pipeline.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
